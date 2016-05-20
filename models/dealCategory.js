35924c5ff5347f38680f74eeba05796eb16555c0
Aakash Khatter <khatteraakash1996@gmail.com>
2016-05-20 08:51:28 +0530

    added DealCategory schema

---
+++ models/dealCategory.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+//Deal Category schema
+var dealCategorySchema = new mongoose.Schema({
+	deal_category_name : {
+		type : String,
+		required : true
+	},
+	templates : [{
+		picture_url : {
+			type : String,
+			required : true
+		}
+	}]
+});
+
+//Creating the model out of the schema
+var DealCategory = mongoose.model('DealCategory', dealCategorySchema);
+
+//Making the model available to our node app
+module.exports = DealCategory;
+
+//**** TO-DO********  Function to get ID of the category, arguments being the name of category
